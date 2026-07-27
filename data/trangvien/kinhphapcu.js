45e37ada1a0eefb2552f79755bf01862e23e4c85
tinokia <ti902188@gmail.com>
2026-07-27 16:27:19 +0700

    Add files via upload

---
+++ data/trangvien/kinhphapcu.js
@@ -0,0 +1,100 @@
+const kinhphapcu = [
+
+{
+    id:1,
+    tieude:"Phẩm Voi - 333",
+    tomtat:"Kinh Pháp Cú - Phẩm Voi - Phần 333",
+    audio:"https://res.cloudinary.com/ohtvoqcu/video/upload/v1785118824/333_dxztwa.m4a",
+    noidung:`Vui thay, già có giới!
+Vui thay, tín an trú!
+Vui thay, được trí tuệ,
+Vui thay, ác không làm.
+`
+},
+{
+    id:2,
+    tieude:"Phẩm Voi - 332",
+    tomtat:"Kinh Pháp Cú - Phẩm Voi - Phần 332",
+    audio:"https://res.cloudinary.com/ohtvoqcu/video/upload/v1785125017/332_rl96ix.m4a",
+    noidung:`
+Vui thay, hiếu kính mẹ,
+Vui thay, hiếu kính cha,
+Vui thay, kính Sa môn,
+Vui thay, kính Hiền Thánh.
+`
+},
+{
+    id:3,
+    tieude:"Phẩm Voi - 330",
+    tomtat:"Kinh Pháp Cú - Phẩm Voi - Phần 330",
+    audio:"https://res.cloudinary.com/ohtvoqcu/video/upload/v1785129353/330_nph8c2.m4a",
+    noidung:`
+Tốt hơn sống một mình,
+Không kết bạn người ngu.
+Ðộc thân, không ác hạnh
+Sống vô tư vô lự,
+Như voi sống rừng voi.
+`
+},
+{
+    id:4,
+    tieude:"Phẩm Voi - 328",
+    tomtat:"Kinh Pháp Cú - Phẩm Voi - Phần 328",
+    audio:"https://res.cloudinary.com/ohtvoqcu/video/upload/v1785140901/328_nsb253.m4a",
+    noidung:`
+Nếu được bạn hiền trí
+Ðáng sống chung, hạnh lành,
+Nhiếp phục mọi hiểm nguy
+Hoan hỷ sống chánh niệm.
+`
+},
+{
+    id:5,
+    tieude:"Phẩm Voi - 322",
+    tomtat:"Kinh Pháp Cú - Phẩm Voi - Phần 322",
+    audio:"https://res.cloudinary.com/ohtvoqcu/video/upload/v1785142089/322_od8xkz.m4a",
+    noidung:`
+Tốt thay, con la thuần,
+Thuần chủng loài ngựa Xinh.
+Ðại tượng, voi có ngà.
+Tự điều mới tối thượng.
+`
+},
+{
+    id:6,
+    tieude:"Phẩm Voi - 325",
+    tomtat:"Kinh Pháp Cú - Phẩm Voi - Phần 325",
+    audio:"https://res.cloudinary.com/ohtvoqcu/video/upload/v1785142594/325_ium6fb.m4a",
+    noidung:`
+Người ưa ngủ, ăn lớn
+Nằm lăn lóc qua lại,
+Chẳng khác heo no bụng,
+Kẻ ngu nhập thai mãi.
+`
+},
+{
+    id:7,
+    tieude:"Phẩm Voi - 324",
+    tomtat:"Kinh Pháp Cú - Phẩm Voi - Phần 324",
+    audio:"https://res.cloudinary.com/ohtvoqcu/video/upload/v1785143233/324_oc61hx.m4a",
+    noidung:`
+Con voi tên Tài Hộ,
+Phát dục, khó điều phục,
+Trói buộc, không ăn uống.
+Voi nhớ đến rừng voi.
+`
+},
+{
+    id:8,
+    tieude:"Phẩm Voi - 331",
+    tomtat:"Kinh Pháp Cú - Phẩm Voi - Phần 331",
+    audio:"https://res.cloudinary.com/ohtvoqcu/video/upload/v1785143875/331_gds3ar.m4a",
+    noidung:`
+Vui thay, bạn lúc cần!
+Vui thay, sống biết đủ,
+Vui thay, chết có đức!
+Vui thay, mọi khổ đoạn.
+`
+},
+];
+
