2536c274c2e32f9b58c100252ff04aee2abb8f45
tinokia <ti902188@gmail.com>
2026-07-29 09:10:05 +0700

    Add files via upload

---
+++ sach.js
@@ -0,0 +1,18 @@
+const sach = [
+
+{
+    id:1,
+    tieude:"Kinh Pháp Cú",
+    tomtat:"Bản dịch tiếng Việt",
+    file:"kinh-phap-cu.md"
+},
+
+{
+    id:2,
+    tieude:"Kinh Kim Cang",
+    tomtat:"Bản dịch tiếng Việt",
+    file:"kinh-kim-cang.md"
+}
+
+];
+
