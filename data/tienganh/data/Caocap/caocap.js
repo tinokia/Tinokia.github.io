596cc7ec789a52c15d0667273baafd614b69912f
tinokia <ti902188@gmail.com>
2026-08-20 16:03:03 +0700

    Add files via upload

---
+++ data/tienganh/data/Caocap/caocap.js
@@ -0,0 +1,37 @@
+const BAI_TAP = {
+  1: [
+    { question: "________ every child in the school passed the swimming test.", A: "Near to", B: "Near by", C: "Next to", D: "Nearly", correct: "D" },
+    { question: "She can look back on her career with great ________.", A: "satisfied", B: "satisfaction", C: "satisfactory", D: "satisfactorily", correct: "B" },
+    { question: "The captain as well as the passengers ________ frightened.", A: "be", B: "been", C: "was", D: "were", correct: "C" },
+    { question: "Ann has got two sisters. Both of them ________ married.", A: "have", B: "has", C: "is", D: "are", correct: "D" },
+    { question: "Each learner of English ________ a good dictionary.", A: "needs", B: "need", C: "is", D: "are", correct: "A" },
+    { question: "Money, nor fame, nor wealth ________ brought happiness to everybody.", A: "has", B: "have", C: "is", D: "are", correct: "A" },
+    { question: "They ________ at this school before they became interpreters.", A: "have taught", B: "had taught", C: "taught", D: "were taught", correct: "B" },
+    { question: "The salary of a bus driver is much higher ________.", A: "in comparison with the salary of a teacher", B: "than a teacher", C: "than that of a teacher", D: "to compare as a teacher", correct: "C" },
+    { question: "Professional people expect you to call them when it is necessary ________ an appointment.", A: "to cancel", B: "canceling", C: "cancil", D: "canceled", correct: "A" },
+    { question: "A computer is usually chosen because of its simplicity of operation and ease of maintenance ________ its capacity to store information.", A: "the same as", B: "the same", C: "as well as", D: "as well", correct: "C" },
+    { question: "In a new culture, many embarrassing situations occur ________ a misunderstanding.", A: "for", B: "because of", C: "because", D: "of", correct: "B" },
+    { question: "Neptune is an extremely cold planet, and ________.", A: "so does Uranus", B: "so has Uranus", C: "so is Uranus", D: "Uranus so", correct: "C" },
+    { question: "________ that gold was discovered at Sutter's Mill and that the California Gold Rush began.", A: "Because in 1848", B: "That in 1848", C: "In 1848 that it was", D: "It was in 1848", correct: "D" },
+    { question: "The job has no ________.", A: "prospectively", B: "prospects", C: "prospector", D: "prospective", correct: "B" },
+    { question: "Frost occurs in valleys and on low grounds ________ on adjacent hills.", A: "more frequently as", B: "as frequently than", C: "more frequently than", D: "frequently than", correct: "C" }
+  ],
+  2: [
+    { question: "Mount Rushmore can be ________ from more than 100 kilometers away.", A: "see", B: "saw", C: "been seen", D: "seen", correct: "D" },
+    { question: "I’d like you ________ my family this weekend.", A: "meet", B: "to meet", C: "meeting", D: "to meeeting", correct: "B" },
+    { question: "Mr. Green, along with his friends, ________ arriving here tonight.", A: "is", B: "are", C: "were", D: "was", correct: "A" },
+    { question: "The statement will be spoken just one time; therefore, you must listen very ________ in order to understand what the speaker has said.", A: "carefully", B: "careful", C: "care", D: "careless", correct: "A" },
+    { question: "Cold objects emit ________ hot objects.", A: "fewer than infrared rays as", B: "fewer infrared rays than", C: "fewer infrared rays as", D: "as fewer infrared rays", correct: "B" },
+    { question: "Gunpowder, in some ways the most effective of all the explosive materials, ________ a mixture of potassium nitrate, charcoal, and sulfur.", A: "were", B: "had been", C: "had", D: "was", correct: "D" },
+    { question: "In the relatively short history of industrial ________ in the United States, New York city has played a vital role.", A: "developed", B: "developing", C: "development", D: "develop", correct: "C" },
+    { question: "As the demand increases, manufacturers who previously produced only a large, luxury car ________ compelled to make a smaller model in order to compete in the market.", A: "is", B: "are", C: "will", D: "should", correct: "B" },
+    { question: "She has ________ the conditions for entry into the college.", A: "satisfied", B: "satisfaction", C: "satisfactory", D: "satisfactorily", correct: "A" },
+    { question: "For the first time in the history of the country, the person ________ was recommended by the president to replace a retiring justice on the supreme court is a woman.", A: "which", B: "whose", C: "who", D: "whom", correct: "C" },
+    { question: "Signor Risponso's fame rested on the breadth of his range, which was ________ any other tenor, living or dead.", A: "greater than that of", B: "as large as", C: "more greater", D: "broader than the one of", correct: "A" },
+    { question: "New words are constantly being invented ________ new objects and concepts.", A: "to describe", B: "a description of", C: "they describe", D: "describe", correct: "A" },
+    { question: "You can’t ________ to learn a foreign language in a week.", A: "expectant", B: "expected", C: "expect", D: "expectation", correct: "C" },
+    { question: "________ rain or snow there are always more than fifty thousand fans at the football games.", A: "Despite", B: "In spite with", C: "Despite of", D: "Despite with", correct: "A" },
+    { question: "The prices of homes are ________ high in urban areas that most young people cannot afford to buy them.", A: "as", B: "so", C: "such", D: "so as", correct: "B" }
+  ]
+
+};
