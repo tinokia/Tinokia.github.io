a5300231d4d6dccaebbe72cb0cae0fcb023bbcc0
tinokia <ti902188@gmail.com>
2026-08-20 16:04:19 +0700

    Add files via upload

---
+++ data/tienganh/data/Grammar/Grammar.js
@@ -0,0 +1,64 @@
+Grammar[1] = [
+  { question: "How often do you play tennis?", A: "On Tuesday.", B: "For two hours.", C: "Almost every day.", D: "With John.", correct: "C" },
+  { question: "Where do you usually eat lunch?", A: "Sandwich.", B: "With Jane.", C: "At 12:00.", D: "In the cafeteria.", correct: "D" },
+  { question: "How long did you study last night?", A: "With Bob.", B: "In my room.", C: "English.", D: "For three hours.", correct: "D" },
+  { question: "What kind of novels do you like?", A: "Yes, I do.", B: "I like spy novels.", correct: "B" },
+  { question: "What kind of work do you do?", A: "I work every day.", B: "I'm a piano teacher.", C: "I worked for two hours.", correct: "B" },
+  { question: "How many hours a day do you watch TV?", A: "About two hours.", B: "In my living room.", C: "I watch the news.", D: "On Tuesday.", correct: "A" },
+  { question: "What is your busiest day of the week?", A: "In the morning.", B: "Every day.", C: "Tuesday.", D: "Last week.", correct: "C" },
+  { question: "My mother is a good cook.", A: "I agree with you.", B: "I agree you.", C: "I agree to you.", D: "I agree for you.", correct: "A" },
+  { question: "What does \"TV\" mean?", A: "For one hour.", B: "Yes", C: "Television.", D: "For one hour.", E: "On Friday.", correct: "C" },
+  { question: "How do you spell \"dog\"?", A: "No", B: "D-O-G", C: "No", D: "I don't", E: "Cat.", F: "I have one dog.", correct: "B" },
+  { question: "What did you do yesterday?", A: "I am swimming.", B: "I swim.", C: "I will swim.", D: "I swam.", correct: "D" },
+  { question: "What do you like to drink?", A: "Coffee.", B: "Saturday evening.", C: "Two.", D: "With my friends.", correct: "A" },
+  { question: "What did you eat last night?", A: "At six.", B: "Spaghetti.", C: "With my family.", D: "At home.", correct: "B" },
+  { question: "What are you doing?", A: "I'm eating.", B: "I ate.", C: "I will be eating.", D: "I have eaten.", correct: "A" },
+  { question: "What will you do this afternoon?", A: "I play soccer.", B: "I played soccer.", C: "I'll play soccer.", D: "I was playing soccer.", correct: "C" },
+  { question: "It seems to me that most restaurants are too expensive?", A: "I don't think it.", B: "I don't think.", C: "I don't think so.", correct: "C" },
+  { question: "Where's Mike?", A: "At school.", B: "At eight.", C: "For three hours.", D: "No, he isn't.", correct: "A" }
+];
+
+Grammar[2] = [
+  { question: "Where do you do your homework?", A: "With John.", B: "In the evening.", C: "About one hour.", D: "Every day.", E: "At home.", correct: "E" },
+  { question: "When did you go to that restaurant?", A: "Speghetti.", B: "With Jane.", C: "Last night.", D: "About 30 minutes.", correct: "C" },
+  { question: "When was the last time you took a picture?", A: "A picture of Jane.", B: "Seven pictures.", C: "About four days ago.", D: "With my camera.", correct: "C" },
+  { question: "What were you doing last night at 7:00?", A: "I sleep.", B: "I slept.", C: "I will be sleeping.", D: "I was sleeping.", correct: "D" },
+  { question: "When will you mail that letter?", A: "Last night.", B: "To Jane.", C: "After school.", correct: "C" },
+  { question: "What are you going to do after dinner?", A: "I took a bath", B: "I'll take a bath.", C: "I take a bath.", correct: "B" },
+  { question: "How long have you been playing the trumpet?", A: "About 50 cm.", B: "For four years.", C: "In my room.", D: "By myself.", correct: "B" },
+  { question: "How many hours a day do you sleep?", A: "I have slept 7 hours.", B: "I am sleeping 7 hours.", C: "I slept 7 hours.", D: "I sleep 7 hours.", correct: "D" },
+  { question: "How often do you write letters?", A: "Two pages.", B: "Two times a week.", C: "Two people.", D: "Two hours.", correct: "B" },
+  { question: "Where can I buy beer?", A: "When you are twenty years old.", B: "About two bottles.", C: "With Jane.", D: "At a liquor store.", correct: "D" },
+  { question: "What's your favorite sport?", A: "Swim.", B: "Swimming.", correct: "B" },
+  { question: "When was the last time you went shopping?", A: "Yesterday.", B: "Tomorrow.", C: "Near the Station.", correct: "A" },
+  { question: "How often do you speak on the telephone?", A: "At least once a day.", B: "In the evening?", C: "For about 30 minutes.", correct: "A" },
+  { question: "How many times have you gone camping?", A: "Three people.", B: "Three days.", C: "Three times.", correct: "C" },
+  { question: "When's your birthday?", A: "November two.", B: "November twice.", C: "November second.", correct: "C" },
+  { question: "Where are you from?", A: "New York.", B: "1982.", C: "At school.", correct: "A" },
+  { question: "How do you get to school?", A: "With train.", B: "In train.", C: "By train.", correct: "C" }
+];
+Grammar[3] = [
+  { question: "When do you write to your friends?", A: "I don't know.", B: "I write to my friends at home.", C: "I do it whenever I have time.", correct: "C" },
+  { question: "How old are you?", A: "I'd rather not say.", B: "I'd rather not tell my husband.", C: "I'd rather not show you.", correct: "A" },
+  { question: "How about going to the theater tonight?", A: "I'm sorry to hear that.", B: "Sorry I have a business meeting tonight.", C: "Sure. Maybe some other time.", correct: "B" },
+  { question: "Can I see your ticket, please?", A: "I'm afraid I can't find it.", B: "No, it's mine.", C: "Yes, it's a ticket.", correct: "A" },
+  { question: "What is your city like?", A: "I like it a lot.", B: "It's small, but nice.", C: "I have no idea.", correct: "B" },
+  { question: "Excuse me, can you tell me where the museum is?", A: "Why do I have to tell you?", B: "You have to know the ropes.", C: "It's at the end of this street.", correct: "C" },
+  { question: "Can I pay by check? I don't have any cash on me.", A: "Sorry, we only take hard cash.", B: "Sorry, the checks were stolen.", C: "Sorry, that's not fair.", correct: "A" },
+  { question: "How would you like your steak?", A: "Rare, please.", B: "A slice, please.", C: "No, thanks.", correct: "A" },
+  { question: "What are you going to do on your summer vacation?", A: "I might have worked hard.", B: "I traveled last summer.", C: "I haven't thought about it.", correct: "C" },
+  { question: "Have you studied English before?", A: "Yes, but I need practice in speaking.", B: "Yes, when I was born.", C: "Yes, I'm interested in your English course.", correct: "A" }
+];
+
+Grammar[4] = [
+  { question: "What time do you get up in the morning?", A: "At seven o'clock.", B: "At seventeen.", C: "At midnight.", correct: "A" },
+  { question: "How long does it take you to get to class?", A: "I take medicime.", B: "It takes fifteen minutes.", C: "I take the bus.", correct: "B" },
+  { question: "Did she leave a message?", A: "No, she said she'd call later.", B: "No, she left the room.", C: "Yes, she'll leave one.", correct: "A" },
+  { question: "May I borrow your car next Thursday? My friend is coming to visit me and I'd like to show her around.", A: "Sure, Thursday was good.", B: "Sure. Any time.", C: "Sure. Maybe some other time.", correct: "B" },
+  { question: "Do you still feel homesick?", A: "Well, I feel dizzy.", B: "Yes, I have problems back home.", C: "Well, I'm getting over it a little.", correct: "C" },
+  { question: "Where can you have your hair cut?", A: "At a hairdresser's.", B: "At a beautiful spot.", C: "At a grocer's.", correct: "A" },
+  { question: "Are you planning to buy an apartment?", A: "No, I just want a big one.", B: "No, I just want to rent one.", C: "Yes, I've already bought one.", correct: "B" },
+  { question: "Don't you have the right change?", A: "No, I only have two nickels.", B: "No, I don't want to look strange.", C: "No, let's change it here.", correct: "A" },
+  { question: "How much do you weigh?", A: "I don't want to wait.", B: "That girl is twice my weight.", C: "I weigh a hundred pounds.", correct: "C" },
+  { question: "Why is Bill so angry?", A: "No wonder. The teacher gave him a good grade.", B: "No wonder. The teacher made him study the verbs.", C: "No wonder. The teacher excused him for being late.", correct: "B" }
+];
